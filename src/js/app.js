0e5755f8e4efaac8825be45ce1b95a63fbd1178a
guillaumelevesque94 <guillaumelevesque666@gmail.com>
2019-10-30 13:30:20 -0400

    Premier commit/push

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+"use strict";
+
+/**
+ *  Ajout de Bootstrap
+**/
+import "bootstrap";
+
+import "../css/style.scss";
+
+
+/* --------------- CODE --------------- */
+console.log("--------------- JS ---------------");
+
+
+import { monComputer, incrementMonComputer } from "./librairie";
+
+console.log(monComputer);
+
+incrementMonComputer();
+
+console.log(monComputer);
+
---
+++ src/js/librairie.js
@@ -0,0 +1,5 @@
+export let monComputer = 3;
+
+export function incrementMonComputer() {
+  monComputer++;
+}
